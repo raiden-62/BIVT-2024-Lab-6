ea1a77ec27fc4e3fe14482abbfd9c644c0596acb
raiden-62 <afedin000@gmail.com>
2025-02-24 19:25:00 +0300

    Removed List and Dictionary

--- Lab6/Purple_5.cs
+++ Lab6/Purple_5.cs
@@ -34,13 +34,13 @@ namespace Lab_6
                     switch (questionNumber) 
                     {
                         case 1:
-                            if (responses[i].Animal != null) answered++;
+                            if (responses[i].Animal != null && responses[i].Animal != "") answered++;
                             break;
                         case 2:
-                            if (responses[i].CharacterTrait != null) answered++;
+                            if (responses[i].CharacterTrait != null && responses[i].CharacterTrait != "") answered++;
                             break;
                         case 3:
-                            if (responses[i].Concept != null) answered++;
+                            if (responses[i].Concept != null && responses[i].Concept != "") answered++;
                             break;
                         default:
                             return 0;
@@ -89,41 +89,63 @@ namespace Lab_6
             public string[] GetTopResponses(int question)
             {
                 if (_responses == null) return null;
-                Dictionary<string, int> responseCount = new Dictionary<string, int>();
+                string[] responses = new string[0];
+                int[] count = new int[0];
+
                 foreach (var response in _responses)
                 {
                     switch (question)
                     {
                         case 1:
-                            if (response.Animal != null)
+                            if (response.Animal != null && response.Animal != "")
                             {
-                                if(!responseCount.ContainsKey(response.Animal)) responseCount.Add(response.Animal, 1);
-                                else responseCount[response.Animal]++;
+                                if (!responses.Contains(response.Animal))
+                                {
+                                    Array.Resize(ref responses, responses.Length + 1);
+                                    Array.Resize(ref count, count.Length + 1);
+                                    responses[responses.Length - 1] = response.Animal;
+                                    count[responses.Length - 1] = 1;
+                                }
+                                else count[Array.IndexOf(responses, response.Animal)]++;
                             }
                             break;
                         case 2:
-                            if (response.CharacterTrait != null)
+                            if (response.CharacterTrait != null && response.CharacterTrait != "")
                             {
-                                if (!responseCount.ContainsKey(response.CharacterTrait)) responseCount.Add(response.CharacterTrait, 1);
-                                else responseCount[response.CharacterTrait]++;
+                                if (!responses.Contains(response.CharacterTrait))
+                                {
+                                    Array.Resize(ref responses, responses.Length + 1);
+                                    Array.Resize(ref count, count.Length + 1);
+                                    responses[responses.Length - 1] = response.CharacterTrait;
+                                    count[responses.Length - 1] = 1;
+                                }
+                                else count[Array.IndexOf(responses, response.CharacterTrait)]++;
                             }
                             break;
                         case 3:
-                            if (response.Concept != null)
+                            if (response.Concept != null && response.Concept != "")
                             {
-                                if (!responseCount.ContainsKey(response.Concept)) responseCount.Add(response.Concept, 1);
-                                else responseCount[response.Concept]++;
+                                if (!responses.Contains(response.Concept))
+                                {
+                                    Array.Resize(ref responses, responses.Length + 1);
+                                    Array.Resize(ref count, count.Length + 1);
+                                    responses[responses.Length - 1] = response.Concept;
+                                    count[responses.Length - 1] = 1;
+                                }
+                                else count[Array.IndexOf(responses, response.Concept)]++;
                             }
                             break;                     
                     }
                 }
 
-                var sortedByResponseCount = responseCount.OrderByDescending(response => response.Value).ToList();
-                string[] topResponses = new string[sortedByResponseCount.Count];
+                DictionariesAreForTheWeak(responses, count);
+
+                string[] topResponses = new string[responses.Length];
                 int i = 0;
-                foreach(var pair in sortedByResponseCount)
+                foreach(var pair in responses)
                 {
-                    topResponses[i++] = pair.Key;
+                    topResponses[i] = responses[i];
+                    i++;
                 }
                 if (topResponses.Length > 5)
                 {
@@ -131,6 +153,25 @@ namespace Lab_6
                 }
 
                 return topResponses;
+            }
+
+            private static void DictionariesAreForTheWeak(string[] keys, int[] values)
+            {
+                for (int i = 1; i < values.Length; i++)
+                {
+                    string key = keys[i];
+                    int value = values[i];
+                    int j = i - 1;
+
+                    while (j >= 0 && values[j] < value)
+                    {
+                        keys[j + 1] = keys[j];
+                        values[j + 1] = values[j];
+                        j--;
+                    }
+                    keys[j + 1] = key;
+                    values[j + 1] = value;
+                }
             }
 
             public void Print()
